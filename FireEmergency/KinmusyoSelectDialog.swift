2e603d7997fd617c499e7fe39a0c88ecc916f478
tadakazu1972 <tadakazu1972@gmail.com>
2021-03-07 17:49:32 +0900

    now sansyusyo collection

--- FireEmergency/KinmusyoSelectDialog.swift
+++ FireEmergency/KinmusyoSelectDialog.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class KinmusyoSelectDialog: NSObject {
+class KinmusyoSelectDialog: NSObject, UICollectionViewDelegate, UICollectionViewDataSource {
     //ボタン押したら出るUIWindow
     fileprivate var parent: PersonalViewController1!
     fileprivate var win1: UIWindow!
@@ -16,13 +16,15 @@ class KinmusyoSelectDialog: NSObject {
     fileprivate var text2: UITextView!
     fileprivate var text3: UITextView!
     fileprivate var text4: UITextView!
-    fileprivate var btnKinmu: UIButton!
-    fileprivate var btnShitei: UIButton!
-    fileprivate var btnSonota: UIButton!
+    fileprivate var collection: UICollectionView!
     fileprivate var items:[String] = []
+    fileprivate var sansyusyo:[String] = []
     fileprivate var address:[String] = []
     fileprivate var mailAddress: String! //メール送信先アドレス格納用
     fileprivate var subject: String! //参集署ごとのメール件名「＠参集署」格納用
+    fileprivate var btnKinmu: UIButton!
+    fileprivate var btnShitei: UIButton!
+    fileprivate var btnSonota: UIButton!
     fileprivate var btnClose: UIButton!
     fileprivate var btnMail: UIButton!
     fileprivate var mSansyusyoSelectDialog: SansyusyoSelectDialog!
@@ -30,6 +32,8 @@ class KinmusyoSelectDialog: NSObject {
     fileprivate var mKyokusyoSelectDialog: KyokusyoSelectDialog!
     //非常参集　職員情報　保存用
     let userDefaults = UserDefaults.standard
+    //警告用
+    fileprivate var mAlertDialog: AlertDialog!
     
     //コンストラクタ
     init(parentView: PersonalViewController1){
@@ -39,24 +43,38 @@ class KinmusyoSelectDialog: NSObject {
         text2 = UITextView()
         text3 = UITextView()
         text4 = UITextView()
-        btnKinmu = UIButton()
-        btnShitei = UIButton()
-        btnSonota = UIButton()
+        
         let layout = UICollectionViewFlowLayout() //これがないとエラーになる
         layout.itemSize = CGSize(width: 100,height: 50) // Cellの大きさ
         layout.sectionInset = UIEdgeInsets(top: 8, left: 32, bottom: 8, right: 32) //Cellのマージン
         layout.minimumInteritemSpacing = 40 //セル同士の間隔
         layout.headerReferenceSize = CGSize(width: 1,height: 1) //セクション毎のヘッダーサイズ
+        collection = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        
+        btnKinmu = UIButton()
+        btnShitei = UIButton()
+        btnSonota = UIButton()
+        
         btnClose = UIButton()
         btnMail  = UIButton()
                 
-        //itemsに参集署を設定
-        items = ["消防局","消防署"]
-
         text1.text = "■参集先"
         text2.text = "勤務消防署"
         text3.text = "指定参集署"
         text4.text = "上記以外"
+        
+        //基礎データから勤務消防署、指定参集署を呼び出し
+        let mainStation: String! = userDefaults.string(forKey: "mainStation")
+        let tsunamiStation: String! = userDefaults.string(forKey: "tsunamiStation")
+        //collectionViewのアイテムとして配列に格納
+        items = [mainStation, tsunamiStation, "上記以外"]
+        
+        //メール送信に使う基礎設定
+        sansyusyo = ["北","都島","福島","此花","中央","西","港","大正","天王寺","浪速","西淀川","淀川","東淀川","東成","生野","旭","城東","鶴見","住之江","阿倍野","住吉","東住吉","平野","西成","水上"]
+        
+        address =
+            ["pa0005","pa0006","pa0007","pa0008","pa0009","pa0036","pa0010","pa0011","pa0013","pa0012","pa0014","pa0015","pa0016","pa0017","pa0018","pa0019","pa0020","pa0021","pa0023","pa0022","pa0024","pa0025","pa0026","pa0027","pa0028"]
+        
     }
     
     //デコンストラクタ
@@ -67,6 +85,7 @@ class KinmusyoSelectDialog: NSObject {
         text2 = nil
         text3 = nil
         text4 = nil
+        collection = nil
         btnKinmu = nil
         btnShitei = nil
         btnSonota = nil
@@ -92,6 +111,7 @@ class KinmusyoSelectDialog: NSObject {
     //表示
     func showInfo (){
         //元の画面を暗く、画面タップ無効化
+        mViewController2.view.alpha = 0.1 //親２画面も明るく
         parent.view.alpha = 0.1
         parent.view.isUserInteractionEnabled = false
         //初期設定
@@ -114,6 +134,14 @@ class KinmusyoSelectDialog: NSObject {
         text1.textAlignment = NSTextAlignment.left
         text1.isEditable = false
         self.win1.addSubview(text1)
+        
+        //UICollectionView生成
+        collection.frame = CGRect(x: 10,y: 60, width: self.win1.frame.width-20, height: self.win1.frame.height-100)
+        collection.backgroundColor = UIColor.white
+        collection.delegate = self
+        collection.dataSource = self
+        collection.register(CustomUICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
+        self.win1.addSubview(collection)
         
         //勤務消防署、指定参集署、上記以外　TextView生成
         text2.frame = CGRect(x: 0, y: 0, width: self.win1.frame.width/2, height: 60)
@@ -143,6 +171,7 @@ class KinmusyoSelectDialog: NSObject {
         text4.isEditable = false
         self.win1.addSubview(text4)
         
+        /*
         //勤務署ボタン生成
         btnKinmu.frame = CGRect(x: 0, y:0, width: 120, height: 48)
         btnKinmu.backgroundColor = UIColor(red:0.85, green:0.85, blue:0.85, alpha:1.0)
@@ -182,6 +211,7 @@ class KinmusyoSelectDialog: NSObject {
         btnSonota.translatesAutoresizingMaskIntoConstraints = false
         btnSonota.addTarget(self, action: #selector(self.onClickSonota(_:)), for: .touchUpInside)
         self.win1.addSubview(btnSonota)
+        */
         
         //閉じるボタン生成
         btnClose.frame = CGRect(x: 0,y: 0,width: 125,height: 30)
@@ -218,6 +248,28 @@ class KinmusyoSelectDialog: NSObject {
         parent.view.alpha = 1.0 //元の画面明るく
         mViewController2.view.alpha = 1.0 //親２画面も明るく
         parent.view.isUserInteractionEnabled = true //タップ有効化
+    }
+    
+    //collectionView処理
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int)-> Int {
+        return self.items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: CustomUICollectionViewCell = collection.dequeueReusableCell(withReuseIdentifier: "MyCell", for: indexPath) as! CustomUICollectionViewCell
+        cell.textLabel?.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //タップした参集署のメールアドレスを格納
+        mailAddress = address[indexPath.row] + "@city.osaka.lg.jp"
+        subject = "@" + items[indexPath.row]
+        //確認用
+        print("セルを選択 #\(indexPath.row)!")
+        print(mailAddress!)
+        print(subject!)
     }
     
     //上記以外参集先到着ボタンクリック
